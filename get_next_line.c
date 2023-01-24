9ce86261104819a2ccce71190758530fd0638754
Serdok1 <sinaeren02@hotmail.com>
2023-01-24 14:21:50 +0300

    norm_fix

--- get_next_line.c
+++ get_next_line.c
@@ -1,114 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sozbayra <sozbayra@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/24 13:32:16 by sozbayra          #+#    #+#             */
+/*   Updated: 2023/01/24 14:19:02 by sozbayra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-char    *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    static char *static_str;
-    char    *str;
+	static char	*static_str;
+	char		*str;
 
-    if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (0);
-    static_str = read_process(fd, static_str);
-    if (!static_str)
-    {
-        free(static_str);
-        return (0);
-    }
-    str = substr_process(static_str);
-    static_str = new_static_str(static_str);
-    return(str);
-}
-
-char    *read_process(int fd, char *static_str)
-{
-    int     fd_info;
-    char    *str;
-
-    fd_info = 1;
-    str = malloc((BUFFER_SIZE + 1) * sizeof(char));
-    if (!str)
-    {
-        free(str);
-		return (NULL);
-    }
-    while(!ft_strchr(static_str, '\n') && fd_info != 0)
-    {
-        fd_info = read(fd, str, BUFFER_SIZE);
-        if (fd_info == -1)
-        {
-            free(str);
-            free(static_str);
-            return (0);
-        }
-        str[fd_info] = '\0';
-        static_str = ft_strjoin(static_str, str);
-
-    }
-    free(str);
-    return(static_str);
-}
-
-char    *substr_process(char *static_str){
-    char    *str;
-    int     i;
-
-    i = 0;
-    if (!*(static_str + i))
-		return (0);
-    while(static_str[i] != '\0' && static_str[i] != '\n')
-        i++;
-    if(static_str[i] == '\n')
-        str = ft_substr(static_str, 0, i + 1);
-    else
-        str = ft_substr(static_str, 0, i);
-    if (!str)
-    {
-        free(str);
-		return (0);
-    }
-    return (str);
-}
-
-char    *new_static_str(char *static_str)
-{
-    char    *new_str;
-    int     i;
-
-    i = 0;
-    while(static_str[i] != '\n' && static_str[i])
-        i++;
-    if (!static_str[i])
+	static_str = read_process(fd, static_str);
+	if (!static_str)
 	{
 		free(static_str);
 		return (0);
 	}
-    i++;
-    new_str = ft_substr(static_str, i, (ft_strlen(static_str) - i));
-    if (!new_str)
-    {
-        free(new_str);
-		return (NULL);
-    }
-    free(static_str);
-    return(new_str);
+	str = substr_process(static_str);
+	static_str = new_static_str(static_str);
+	return (str);
 }
 
-/* #include <fcntl.h>
-# include <stdlib.h>
-# include <unistd.h>
-int main()
+char	*read_process(int fd, char *static_str)
 {
-    int fd;
-    int i;
+	int		fd_info;
+	char	*str;
 
-    i = 1;
-    fd = open("deneme.txt", O_RDONLY);
-    while(i <= 3)
-    {
-        printf("gnl: %s \n", get_next_line(fd));
-		printf("\n");
-		i++;
-    }
-    system("leaks a.out");
-	return(0);
+	fd_info = 1;
+	str = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!str)
+	{
+		free(str);
+		return (NULL);
+	}
+	while (!ft_strchr(static_str, '\n') && fd_info != 0)
+	{
+		fd_info = read(fd, str, BUFFER_SIZE);
+		if (fd_info == -1)
+		{
+			free(str);
+			free(static_str);
+			return (0);
+		}
+		str[fd_info] = '\0';
+		static_str = ft_strjoin(static_str, str);
+	}
+	free(str);
+	return (static_str);
 }
- */
+
+char	*substr_process(char *static_str)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	if (!*(static_str + i))
+		return (0);
+	while (static_str[i] != '\0' && static_str[i] != '\n')
+		i++;
+	if (static_str[i] == '\n')
+		str = ft_substr(static_str, 0, i + 1);
+	else
+		str = ft_substr(static_str, 0, i);
+	if (!str)
+	{
+		free(str);
+		return (0);
+	}
+	return (str);
+}
+
+char	*new_static_str(char *static_str)
+{
+	char	*new_str;
+	int		i;
+
+	i = 0;
+	while (static_str[i] != '\n' && static_str[i])
+		i++;
+	if (!static_str[i])
+	{
+		free(static_str);
+		return (0);
+	}
+	i++;
+	new_str = ft_substr(static_str, i, (ft_strlen(static_str) - i));
+	if (!new_str)
+	{
+		free(new_str);
+		return (NULL);
+	}
+	free(static_str);
+	return (new_str);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sozbayra <sozbayra@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/24 14:19:11 by sozbayra          #+#    #+#             */
+/*   Updated: 2023/01/24 14:19:35 by sozbayra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -12,7 +24,6 @@ char			*get_next_line(int fd);
 unsigned int	ft_strlen(char *ptr);
 char			*ft_strchr(char *s, int c);
 char			*ft_strdup(char *s1);
-int				ft_chrcheck(char *s, int c);
 char			*ft_substr(char *s, unsigned int start, unsigned int len);
 char			*ft_strjoin(char *s1, char *s2);
 char			*new_static_str(char *static_str);
